ddd42b8380ea7e6be4012e76047d434192324131
TaufiqDatau <taufiqhdatau@gmail.com>
2024-10-11 23:49:03 +0700

    fixing tsserver error

--- lua/taufiqDatau/plugins/treesitter.lua
+++ lua/taufiqDatau/plugins/treesitter.lua
@@ -22,7 +22,6 @@ return {
       },
       -- ensure these language parsers are installed
       ensure_installed = {
-        "tsserver",
         "json",
         "javascript",
         "typescript",
