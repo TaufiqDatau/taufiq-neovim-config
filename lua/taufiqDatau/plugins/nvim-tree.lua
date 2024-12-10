766d75d1635772daa6ed2d53e38bd1d7bd5b6d76
TaufiqDatau <taufiqhdatau@gmail.com>
2024-12-10 14:30:48 +0700

    add-terminal-and-smear-animation-to-cursor

--- lua/taufiqDatau/plugins/nvim-tree.lua
+++ lua/taufiqDatau/plugins/nvim-tree.lua
@@ -1,8 +1,8 @@
-return{
+return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-local nvimtree = require("nvim-tree")
+    local nvimtree = require("nvim-tree")
     -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
@@ -51,5 +51,5 @@ local nvimtree = require("nvim-tree")
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
